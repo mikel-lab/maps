a2b9f7f5c10ba1ac4bc0dd0d8fd5fa58a4382887
Mikel <mikelcobian@gmail.com>
2024-04-23 16:52:08 +0200

    mvvm structure and basic data type model created

---
+++ MapApp/MapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  MapApp
+//
+//  Created by Mikel Cobian on 23/4/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+	
+	let name: String
+	let cityName: String
+	let coordinates: CLLocationCoordinate2D
+	let description: String
+	let imageNames: [String]
+	let link: String
+	
+}
