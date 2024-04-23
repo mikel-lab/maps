abb54cd7395eb8cbc0c8bf8ea50da7264379285b
Mikel <mikelcobian@gmail.com>
2024-04-23 16:36:10 +0200

    App creation push

---
+++ MapApp/MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Mikel Cobian on 23/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
